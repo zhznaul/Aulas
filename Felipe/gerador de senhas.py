c864d01c28ce2b84ce90a328e5a78eb51ce4ffc7
Luanzhz <00001107654038sp@al.educacao.sp.gov.br>
2024-11-14 15:17:58 -0300

    Add files via upload

---
+++ Felipe/gerador de senhas.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+def gerar_senha(tamanho=12):
+    caracteres = string.ascii_letters + string.digits + string.punctuation
+    senha = ''.join(random.choice(caracteres) for _ in range(tamanho))
+    return senha
+print(gerar_senha())
